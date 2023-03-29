40c38701879d006e731a2a95a980e8e216d2fa05
Eovone <114399255+Eovone@users.noreply.github.com>
2023-03-29 14:32:52 +0200

    asdv

---
+++ KyhTestingStartingCase/ShopAdmin/Commands/VerifyImage.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ShopGeneral.Data;
+using System.Text;
+
+namespace ShopAdmin.Commands
+{
+    public class VerifyImage : ConsoleAppBase
+    {
+        private readonly ApplicationDbContext _context;
+        public VerifyImage(DbContextOptions<ApplicationDbContext> janne)
+        {
+            _context = new ApplicationDbContext(janne);
+
+            foreach (var product in _context.Products)
+            {
+                if (product.ImageUrl is null)
+                {
+                    File.WriteAllText($"\\outfiles\\products\\missingimages-{GetDateToday()}.txt", product.Id.ToString());
+                }
+            }
+        }
+
+        //public void VerifyTheseImages(List<Product> productList)
+        //{
+        //    foreach (var product in productList)
+        //    {
+        //        if (product.ImageUrl is null)
+        //        {
+        //            File.WriteAllText($"\\outfiles\\products\\missingimages-{GetDateToday()}.txt", product.Id.ToString());
+        //        }
+        //    }
+        //}
+
+        public string GetDateToday()
+        {
+            var yyyyMMdd = new StringBuilder();
+            yyyyMMdd.Append(DateTime.Now.Year);
+            yyyyMMdd.Append(DateTime.Now.Month);
+            yyyyMMdd.Append(DateTime.Now.Day);
+            return yyyyMMdd.ToString();
+        }
+        
+
+    }
+}
