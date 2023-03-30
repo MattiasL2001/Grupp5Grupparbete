74a9ae12c455986625318575ff6b93aa8a80c979
Eovone <114399255+Eovone@users.noreply.github.com>
2023-03-30 13:48:25 +0200

    asd

---
+++ KyhTestingStartingCase/ShopAdmin/Commands/VerifyCategory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopGeneral.Data;
+
+namespace ShopAdmin.Commands
+{
+    public class Verifycategory : ConsoleAppBase
+    {
+        private readonly ApplicationDbContext _context;
+        public Verifycategory(DbContextOptions<ApplicationDbContext> options)
+        {
+            _context = new ApplicationDbContext(options);
+        }
+
+        public void Verifycatagorieshasproducts()
+        {
+            WriteToFile(ListCategoriesWithNoProductMatch(_context.Categories.ToList(), _context.Products.ToList()));
+        }
+        public List<string> ListCategoriesWithNoProductMatch(List<Category> categoryList, List<Product> products)
+        {
+            List<string> listOfCategoriesWithNoProducts = new List<string>();
+            foreach (var category in categoryList)
+            {
+                var thisManyProductsInThisCategory = products.Where(pr => pr.Category.Name == category.Name).Count();
+
+                if (thisManyProductsInThisCategory == 0) { listOfCategoriesWithNoProducts.Add(category.Name); }               
+            }
+
+            return listOfCategoriesWithNoProducts;
+        }
+        public void WriteToFile(List<string> listOfCategoriesWithNoProducts)
+        {
+            var folderPath = "..\\outfiles\\category\\";
+            if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+            File.WriteAllLines($"{folderPath}missingproducts-{GetDateToday()}.txt", listOfCategoriesWithNoProducts);
+        }
+
+        public string GetDateToday()
+        {
+            return DateTime.Now.ToString("yyyyMMdd");
+        }
+    }
+}
---
+++ KyhTestingStartingCase/ShopAdminTesting/VerifyCategoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAdmin.Commands;
+using ShopGeneral.Data;
+
+namespace ShopAdminTesting
+{
+    [TestClass]
+    public class VerifyCategoryTests
+    {
+        private readonly Verifycategory sut;
+        public VerifyCategoryTests()
+        {
+            sut = new Verifycategory(new DbContextOptions<ApplicationDbContext>());
+        }
+
+        [TestMethod]
+        public void Check_date_of_today_is_in_correct_format()
+        {
+            var result = sut.GetDateToday();
+            var expected = DateTime.Now.ToString("yyyyMMdd");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void When_category_has_no_products_should_write_to_file()
+        {
+            List<string> categoryNameList = new List<string> { "Wagon" };
+
+            sut.WriteToFile(categoryNameList);
+            var folderPath = "..\\outfiles\\category\\";
+            var result = File.ReadAllLines($"{folderPath}missingproducts-{sut.GetDateToday()}.txt");
+
+            Assert.AreEqual("Wagon", result[0]);
+        }
+
+        [TestMethod]
+        public void If_category_has_no_products_should_return_list_of_category_name()
+        {
+            List<Category> listCategory = new List<Category>();
+            var cate1 = new Category { Name = "cate1" };
+            listCategory.Add(cate1);
+            var cate2 = new Category { Name = "cate2" };
+            listCategory.Add(cate2);
+
+            List<Product> listProduct = new List<Product>();
+            var prod1 = new Product { Name = "1", Category = cate2};
+            listProduct.Add(prod1);
+
+            var resultList = sut.ListCategoriesWithNoProductMatch(listCategory, listProduct);
+
+            Assert.AreEqual("cate1", resultList[0]);
+        }
+    }
+}
