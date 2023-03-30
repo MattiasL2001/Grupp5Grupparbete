2861958d5439993f26028383deffc5ada655c263
Eovone <114399255+Eovone@users.noreply.github.com>
2023-03-30 09:23:14 +0200

    asd

--- KyhTestingStartingCase/ShopAdmin/Commands/VerifyImage.cs
+++ KyhTestingStartingCase/ShopAdmin/Commands/VerifyImage.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShopGeneral.Data;
+using System.Net;
 using System.Text;
 
 namespace ShopAdmin.Commands
@@ -18,20 +19,34 @@ namespace ShopAdmin.Commands
 
             foreach (var product in _context.Products)
             {
-                if (product.ImageUrl is null) { listOfProductsWithoutImage.Add(product.Id.ToString()); }
+                if (!DoesImageExist(product.ImageUrl)) { listOfProductsWithoutImage.Add(product.Id.ToString()); }
             }
 
             WriteToFile(listOfProductsWithoutImage);
         }
 
-        public void WriteToFile(List<string> listOfProductsWithoutImage)
+        private bool DoesImageExist(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if(response.StatusCode is HttpStatusCode.NotFound)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void WriteToFile(List<string> listOfProductsWithoutImage)
         {
             var folderPath = "..\\outfiles\\products\\";
             if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
             File.WriteAllLines($"{folderPath}missingimages-{GetDateToday()}.txt", listOfProductsWithoutImage);
         }
 
-        public string GetDateToday()
+        private string GetDateToday()
         {
             var yyyyMMdd = new StringBuilder();
             yyyyMMdd.Append(DateTime.Now.Year);
