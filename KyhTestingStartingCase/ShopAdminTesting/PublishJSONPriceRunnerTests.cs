51d5689f2fb05940db1eda3178a74e6b3159357b
Mattias Lindblad <mattias-lindblad2001@hotmail.com>
2023-04-05 13:26:26 +0200

    combined m/s and main branch

---
+++ KyhTestingStartingCase/ShopAdminTesting/PublishJSONPriceRunnerTests.cs
@@ -0,0 +1,22 @@
+﻿using MvcSuperShop;
+using ShopAdmin;
+using ShopGeneral.Data;
+
+namespace ShopAdminTesting
+{
+    [TestClass]
+    public class PublishJSONPriceRunnerTests
+    {
+        private readonly PublishJSONPriceRunner sut;
+        public PublishJSONPriceRunnerTests() { sut = new PublishJSONPriceRunner(); }
+
+        [TestMethod]
+        public void ReturnJSON()
+        {
+            var p = new Product();
+            var pString = $"id: {p.Id} \ntitle: {p.Name} \ndescription: ";
+            Console.WriteLine(pString);
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
